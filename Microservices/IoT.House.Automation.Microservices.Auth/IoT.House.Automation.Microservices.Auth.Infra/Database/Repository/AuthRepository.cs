1061d0b504cdffe913a5fde75c4b780fff819ade
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2020-04-27 22:12:36 -0300

    feat: Added mocked data
    - Mocked data while implementation with database is not established

--- Microservices/IoT.House.Automation.Microservices.Auth/IoT.House.Automation.Microservices.Auth.Infra/Database/Repository/AuthRepository.cs
+++ Microservices/IoT.House.Automation.Microservices.Auth/IoT.House.Automation.Microservices.Auth.Infra/Database/Repository/AuthRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Text;
 using IoT.House.Automation.Microservices.Auth.Domain.Interfaces;
 using IoT.House.Automation.Microservices.Auth.Domain.Models;
@@ -9,14 +10,34 @@ namespace IoT.House.Automation.Microservices.Auth.Infra.Database.Repository
 {
     public class AuthRepository : IAuthRepository
     {
+        private readonly List<User> _users = new List<User>
+        {
+            new User {Email = "something1@test.com", Username = "test1"},
+            new User {Email = "something2@test.com", Username = "test2"}
+        };
+
         public bool IsCredentialsValid(Login login)
         {
-            throw new NotImplementedException();
+            return _users.Any(u => u.Username.Equals(login.Username));
         }
 
         public DataSet GetUserInformation(string username)
         {
-            throw new NotImplementedException();
+            var ds = new DataSet();
+            var dt = new DataTable();
+
+            dt.Columns.Add("Email");
+            dt.Columns.Add("Username");
+
+            var result = _users.FirstOrDefault(u => u.Username.Equals(username));
+
+            if (result != null)
+            {
+                dt.Rows.Add(result.Email, result.Username);
+            }
+
+            ds.Tables.Add(dt);
+            return ds;
         }
     }
 }
