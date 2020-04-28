b9d79f21a544c71a8f9944e5656a84fd6f75c014
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2020-04-27 22:12:59 -0300

    feat: Implemented auth controller

--- Microservices/IoT.House.Automation.Microservices.Auth/IoT.House.Automation.Microservices.Auth.Api/Controllers/AuthController.cs
+++ Microservices/IoT.House.Automation.Microservices.Auth/IoT.House.Automation.Microservices.Auth.Api/Controllers/AuthController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IoT.House.Automation.Libraries.Mapper.Abstractions;
 using IoT.House.Automation.Microservices.Auth.Domain.Interfaces;
+using IoT.House.Automation.Microservices.Auth.Domain.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,16 +15,38 @@ namespace IoT.House.Automation.Microservices.Auth.Api.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuth _authService;
+        private readonly IMapper _mapper;
 
-        public AuthController(IAuth authService)
+        public AuthController(IAuth authService, IMapper mapper)
         {
             _authService = authService;
+            _mapper = mapper;
         }
 
-        [HttpGet]
-        public ActionResult Get()
+        [HttpPost("signin")]
+        public ActionResult SignIn(LoginViewModel login)
         {
-            return new JsonResult(new {Teste = "Testando"});
+            var mapped = _mapper.Map<LoginViewModel, Login>(login);
+            var result = _authService.Signin(mapped);
+            return new JsonResult(result);
+        }
+
+        [HttpPost("user")]
+        public ActionResult GetUserFromToken([FromBody] string token)
+        {
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return BadRequest("Token cannot be empty");
+            }
+
+            var result = _authService.GetSignedUser(token);
+
+            if (result == null)
+            {
+                return BadRequest("User with this token cannot be found");
+            }
+
+            return new JsonResult(result);
         }
     }
 }
---
+++ Microservices/IoT.House.Automation.Microservices.Auth/IoT.House.Automation.Microservices.Auth.Api/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IoT.House.Automation.Microservices.Auth.Api
+{
+    public class LoginViewModel
+    {
+        [FromBody]
+        public string Username { get; set; }
+        [FromBody]
+        public string Password { get; set; }
+    }
+}
