674f1255bbd0894d2bd20380f20c4b0c1a0e51d0
Katsshura <38816951+Katsshura@users.noreply.github.com>
2020-07-04 00:46:17 -0300

    feat: config of dependency injection

--- Microservices/IoT.House.Automation.Microservices.Arduino/IoT.House.Automation.Microservices.Arduino.Api/Startup.cs
+++ Microservices/IoT.House.Automation.Microservices.Arduino/IoT.House.Automation.Microservices.Arduino.Api/Startup.cs
@@ -6,6 +6,7 @@ using IoT.House.Automation.Libraries.Mapper;
 using IoT.House.Automation.Libraries.Mapper.Abstractions;
 using IoT.House.Automation.Microservices.Arduino.Application.Services;
 using IoT.House.Automation.Microservices.Arduino.Domain.Interfaces;
+using IoT.House.Automation.Microservices.Arduino.Infra.Mongo;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +14,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
 
 namespace IoT.House.Automation.Microservices.Arduino.Api
 {
@@ -30,6 +33,7 @@ namespace IoT.House.Automation.Microservices.Arduino.Api
         {
             ConfigureDomainServices(services);
             ConfigureExternalServices(services);
+            ConfigureMongoDbServices(services);
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -48,11 +52,23 @@ namespace IoT.House.Automation.Microservices.Arduino.Api
         private void ConfigureDomainServices(IServiceCollection services)
         {
             services.AddSingleton<IMap, MapService>();
+            services.AddScoped<IArduino, ArduinoRepository>();
         }
 
         private void ConfigureExternalServices(IServiceCollection services)
         {
             services.AddSingleton<IMapper, MapperService>();
+        }
+
+        private void ConfigureMongoDbServices(IServiceCollection services)
+        {
+            services.AddSingleton<IMongoClient>(p =>
+            {
+                var client = new MongoClient(Configuration.GetConnectionString("MongoAccess"));
+                var pack = new ConventionPack { new IgnoreExtraElementsConvention(true) };
+                ConventionRegistry.Register("My Solution Conventions", pack, t => true);
+                return client;
+            });
         }
     }
 }
