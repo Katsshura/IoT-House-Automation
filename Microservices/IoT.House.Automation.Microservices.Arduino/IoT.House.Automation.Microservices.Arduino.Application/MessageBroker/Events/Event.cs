556d1247853d92b4828209da3d689c5da3d3f911
Katsshura <38816951+Katsshura@users.noreply.github.com>
2020-07-06 01:11:27 -0300

    feat: added message bus events

---
+++ Microservices/IoT.House.Automation.Microservices.Arduino/IoT.House.Automation.Microservices.Arduino.Application/MessageBroker/Events/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoT.House.Automation.Microservices.Arduino.Application.MessageBroker.Events
+{
+    public abstract class Event
+    {
+        public Guid Id { get; }
+        public DateTime CreatedAt { get; }
+
+        public abstract Type Type { get; }
+
+        protected Event()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Microservices/IoT.House.Automation.Microservices.Arduino/IoT.House.Automation.Microservices.Arduino.Application/MessageBroker/Handlers/ArduinoAddedEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using IoT.House.Automation.Microservices.Arduino.Application.Interfaces;
+using IoT.House.Automation.Microservices.Arduino.Application.MessageBroker.Events;
+
+namespace IoT.House.Automation.Microservices.Arduino.Application.MessageBroker.Handlers
+{
+    public class ArduinoAddedEventHandler : IEventHandler<ArduinoAddedEvent>
+    {
+        public Task Handle(ArduinoAddedEvent @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
