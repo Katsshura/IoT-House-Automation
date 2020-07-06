6b9ddba88feb6c810902a8ddc695087f308c944b
Katsshura <38816951+Katsshura@users.noreply.github.com>
2020-07-06 01:10:49 -0300

    feat: added converter settings for json converter

---
+++ Microservices/IoT.House.Automation.Microservices.Arduino/IoT.House.Automation.Microservices.Arduino.Application/Util/NewtonsoftJsonUtil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IoT.House.Automation.Microservices.Arduino.Application.Converters;
+using Newtonsoft.Json;
+
+namespace IoT.House.Automation.Microservices.Arduino.Application.Util
+{
+    public static class NewtonsoftJsonUtil
+    {
+        public static JsonSerializerSettings UseCustomNewtonsoftSettings()
+        {
+            var settings = new JsonSerializerSettings();
+            settings.Converters.Add(new IPAddressConverter());
+            settings.Converters.Add(new IPEndPointConverter());
+            return settings;
+        }
+    }
+}
