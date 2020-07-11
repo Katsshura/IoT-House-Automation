60d62d56d24329437aabfdec03bd26dcd44a1409
Katsshura <38816951+Katsshura@users.noreply.github.com>
2020-07-11 03:06:54 -0300

    feat: implemented sync service

---
+++ Microservices/IoT.House.Automation.Microservices.Arduino/IoT.House.Automation.Microservices.Arduino.Sync/Service/SyncService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IoT.House.Automation.Microservices.Arduino.Application.Interfaces;
+using IoT.House.Automation.Microservices.Arduino.Application.MessageBroker.Events;
+using IoT.House.Automation.Microservices.Arduino.Sync.Handlers;
+
+namespace IoT.House.Automation.Microservices.Arduino.Sync.Service
+{
+    public class SyncService : ISync
+    {
+        private readonly IEventBus _eventBus;
+
+        public SyncService(IEventBus eventBus)
+        {
+            _eventBus = eventBus;
+        }
+
+        public void Start()
+        {
+            SubscribeToBus();
+        }
+
+        private void SubscribeToBus()
+        {
+            _eventBus.Subscribe<ArduinoRemovedEvent, ArduinoRemovedEventHandler>();
+        }
+    }
+}
