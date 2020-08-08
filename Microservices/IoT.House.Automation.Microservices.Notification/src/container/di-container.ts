d09c668a881062bd6a94263e2df786ebc59a52c8
Katsshura <38816951+Katsshura@users.noreply.github.com>
2020-08-08 02:54:53 -0300

    feat: added configuration to dependency injection

--- Microservices/IoT.House.Automation.Microservices.Notification/src/container/di-container.ts
+++ Microservices/IoT.House.Automation.Microservices.Notification/src/container/di-container.ts
@@ -5,25 +5,58 @@ import { IEventBus } from "../domain/interfaces/IEventBus";
 import getDecorators from "inversify-inject-decorators";
 import { Types } from "./types";
 import { SubscriptionManager } from "../infra/rabbitmq/subscriptionManager";
+import { IPushSender } from "../domain/interfaces/IPushSender";
+import { FCMSender } from "../infra/firebase/FCMSender";
+import { IEventHandler } from "../domain/interfaces/IEventHandler";
+import { PushNotification } from "../domain/entities/models/pushNotification";
+import { PushNotificationEvent } from "../domain/entities/events/pushNotificationEvent";
+import { PushNotificationEventHandler } from "../domain/handlers/pushNotificationEventHandler";
+import { FCMConnection } from "../infra/firebase/FCMConnection";
 
 export class DependencyResolver {
-    private static DIContainer: Container;
+  private static DIContainer: Container;
 
-    private constructor() { }
+  private constructor() {}
 
-    public static getInstance() : Container {
-
-        if(!this.DIContainer){
-            this.DIContainer = new Container();
-            this.ConfigureContainer();
-        }
-
-        return this.DIContainer;
+  public static getInstance(): Container {
+    if (!this.DIContainer) {
+      this.DIContainer = new Container();
+      this.ConfigureContainer();
     }
 
-    private static ConfigureContainer() {
-        this.DIContainer.bind<IEventBus>(Types.IEventBus).to(RabbitMqEventBus).inSingletonScope();
-        this.DIContainer.bind<SubscriptionManager>(SubscriptionManager).toSelf().inSingletonScope();
-        this.DIContainer.bind<RabbitMqConnection>(RabbitMqConnection).toSelf().inSingletonScope();
-    }
+    return this.DIContainer;
+  }
+
+  private static ConfigureContainer() {
+    this.ConfigureMessageBroker();
+    this.ConfigureFCM();
+    this.ConfigureHandlers();
+  }
+
+  private static ConfigureMessageBroker() {
+    this.DIContainer.bind<IEventBus>(Types.IEventBus)
+      .to(RabbitMqEventBus)
+      .inSingletonScope();
+    this.DIContainer.bind<SubscriptionManager>(SubscriptionManager)
+      .toSelf()
+      .inSingletonScope();
+    this.DIContainer.bind<RabbitMqConnection>(RabbitMqConnection)
+      .toSelf()
+      .inSingletonScope();
+  }
+
+  private static ConfigureFCM() {
+    this.DIContainer.bind<FCMConnection>(FCMConnection)
+      .toSelf()
+      .inSingletonScope();
+    this.DIContainer.bind<IPushSender>(Types.IPushSender)
+      .to(FCMSender)
+      .inSingletonScope();
+  }
+
+  private static ConfigureHandlers() {
+    this.DIContainer.bind<PushNotificationEventHandler>(Types.IEventHandler)
+      .to(PushNotificationEventHandler)
+      .inSingletonScope();
+  }
 }
