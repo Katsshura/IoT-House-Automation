ce47ac0ef91cf53aca11cbf7798cf4375cb085af
Katsshura <38816951+Katsshura@users.noreply.github.com>
2020-08-16 19:29:06 -0300

    feat: implemented contract for lower casing json keys

---
+++ Microservices/IoT.House.Automation.Microservices.Arduino/IoT.House.Automation.Microservices.Arduino.Application/Util/LowerCaseContractResolver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Serialization;
+
+namespace IoT.House.Automation.Microservices.Arduino.Application.Util
+{
+    public class LowerCaseContractResolver : DefaultContractResolver
+    {
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName) || char.IsLower(propertyName, 0))
+                return propertyName;
+
+            return char.ToLowerInvariant(propertyName[0]) + propertyName.Substring(1);
+        }
+    }
+}
