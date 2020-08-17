0f2ccf1d0be5d5514e66f504fe1cfed1fca12f03
Katsshura <38816951+Katsshura@users.noreply.github.com>
2020-08-16 21:38:38 -0300

    refactor: removed java 14 preview and changed property

--- Microservices/IoT.House.Automation.Microservices.Mobile/domain/src/main/java/iot/house/automation/microservices/mobile/domain/models/arduino/events/Event.java
+++ Microservices/IoT.House.Automation.Microservices.Mobile/domain/src/main/java/iot/house/automation/microservices/mobile/domain/models/arduino/events/Event.java
@@ -1,13 +1,12 @@
 package iot.house.automation.microservices.mobile.domain.models.arduino.events;
 
-import iot.house.automation.microservices.mobile.domain.enums.EventInputType;
 import lombok.Getter;
 
 @Getter
 public class Event {
     private String name;
     private String description;
-    private EventInputType expectedInputType;
+    private String expectedInputType;
     private String type;
 
     public Event() {
