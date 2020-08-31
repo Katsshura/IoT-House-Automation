e7e744f7fb303259f503f1c6047aac8522e6595c
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2020-08-31 14:41:35 -0300

    refactor: replaced manual input for spring dynamic input
    replaced manual constructor config input for spring property injection on application.properties

--- Microservices/IoT.House.Automation.Microservices.Mobile/infrastructure/src/main/java/iot/house/automation/microservices/mobile/infra/rabbitMQ/config/RabbitMQConfig.java
+++ Microservices/IoT.House.Automation.Microservices.Mobile/infrastructure/src/main/java/iot/house/automation/microservices/mobile/infra/rabbitMQ/config/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package iot.house.automation.microservices.mobile.infra.rabbitMQ.config;
 
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,13 +11,18 @@ public class RabbitMQConfig {
     private final String publishExchange;
     private final String listenerExchange;
     private final String exchangeType;
-    private final int retryCount;
+    private int retryCount;
 
-    public RabbitMQConfig() {
-        uri = "";
-        listenerExchange = "Arduino";
-        publishExchange = "Mobile";
-        exchangeType = "direct";
-        retryCount = 3;
+    public RabbitMQConfig(@Value("${spring.rabbitmq.uri}") String uri,
+                          @Value("${spring.rabbitmq.publisherExchange}") String publishExchange,
+                          @Value("${spring.rabbitmq.listenerExchange}") String listenerExchange,
+                          @Value("${spring.rabbitmq.exchangeType}") String exchangeType,
+                          @Value("${spring.rabbitmq.retryCount}") int retryCount)
+    {
+        this.uri = uri;
+        this.publishExchange = publishExchange;
+        this.listenerExchange = listenerExchange;
+        this.exchangeType = exchangeType;
+        this.retryCount = retryCount;
     }
 }
