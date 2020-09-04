668ef46708abdf685fdcbf1c05140dff17fbb1ee
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2020-09-04 01:59:26 -0300

    feat: added arduino device adapter and requester

---
+++ Microservices/IoT.House.Automation.Microservices.Arduino/IoT.House.Automation.Microservices.Arduino.Domain/Interfaces/IEventTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IoT.House.Automation.Microservices.Arduino.Domain.Models;
+
+namespace IoT.House.Automation.Microservices.Arduino.Domain.Interfaces
+{
+    public interface IEventTrigger
+    {
+        void Emit(ArduinoInfo to, string eventName, object eventValue);
+    }
+}
