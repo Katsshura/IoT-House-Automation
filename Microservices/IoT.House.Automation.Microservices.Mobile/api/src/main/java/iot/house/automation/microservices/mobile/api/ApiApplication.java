e09d5588cf134e8aacb1b45915220b10e2c295c7
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2020-09-06 04:14:58 -0300

    feat: added dependencies and enabled mongo repositories

--- Microservices/IoT.House.Automation.Microservices.Mobile/api/src/main/java/iot/house/automation/microservices/mobile/api/ApiApplication.java
+++ Microservices/IoT.House.Automation.Microservices.Mobile/api/src/main/java/iot/house/automation/microservices/mobile/api/ApiApplication.java
@@ -2,7 +2,9 @@ package iot.house.automation.microservices.mobile.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories(basePackages = "iot.house.automation.microservices")
 @SpringBootApplication(scanBasePackages = "iot.house.automation.microservices")
 public class ApiApplication {
 
